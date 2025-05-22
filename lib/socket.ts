53ba236613e6d3cc39f5d68127f37b445925ba97
Owen Beck <oebeck@gmail.com>
2025-05-22 15:35:56 -0400

    Only one socket is created

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,14 @@
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === "production" ? undefined : "http://localhost:4000";
 
-export const socket = io(URL);
+let s = (globalThis as any as { socket: Socket | undefined }).socket;
+if (!s) {
+  s = io(URL);
+
+  (globalThis as any as { socket: Socket | undefined }).socket = s;
+}
+
+export const socket = s;
