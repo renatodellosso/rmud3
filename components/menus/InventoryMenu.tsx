4172ed85929f0f64ac4f38a73a82a1b17d8402a3
renatodellosso <rddellosso4@gmail.com>
2025-06-19 15:36:20 -0400

    Add item tooltips

--- components/menus/InventoryMenu.tsx
+++ components/menus/InventoryMenu.tsx
@@ -1,13 +1,15 @@
 import items from "lib/gamedata/items";
 import { PlayerInstance } from "../../lib/types/player";
+import ItemTooltip from "../ItemTooltip";
+
 export default function InventoryMenu({ self }: { self: PlayerInstance }) {
   const inventory = self.inventory;
 
   return (
     <div className="border w-1/6 flex flex-col gap-2">
       <h1 className="text-xl">
-        Inventory ({inventory.getUsedWeight()}/
-        {inventory.getMaxWeightFromPlayer(self) ?? "∞"} kg)
+        Inventory ({inventory.getUsedWeight()}/{inventory.getMaxWeight() ?? "∞"}{" "}
+        kg)
       </h1>
       <table>
         <thead>
@@ -18,9 +20,12 @@ export default function InventoryMenu({ self }: { self: PlayerInstance }) {
           </tr>
         </thead>
         <tbody>
-          {inventory.items.map((item, index) => (
+          {inventory.getItems().map((item, index) => (
             <tr key={index} className="hover:bg-gray-900">
-              <td>{items[item.definitionId].name}</td>
+              <td className="tooltip">
+                {items[item.definitionId].name}
+                <ItemTooltip item={item} />
+              </td>
               <td>{item.amount}</td>
               <td>{items[item.definitionId].weight}</td>
             </tr>
